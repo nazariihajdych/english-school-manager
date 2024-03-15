1e7acf19bb678c3f7d12ff3d43b592b4d22b8b6e
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-15 18:37:29 +0200

    add entityScan and Hibernate properties

--- school-manager-web/src/main/java/ua/english/school/EnglishSchoolManagerApplication.java
+++ school-manager-web/src/main/java/ua/english/school/EnglishSchoolManagerApplication.java
@@ -2,8 +2,10 @@ package ua.english.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "ua.english.school")
 public class EnglishSchoolManagerApplication {
 
 	public static void main(String[] args) {
