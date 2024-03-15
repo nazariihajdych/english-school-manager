e21e2ae651771a396d87dde8af4d1a3b6ae50cc3
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-15 16:23:33 +0200

    first draft

---
+++ src/main/java/ua/english/school/EnglishSchoolManagerApplication.java
@@ -0,0 +1,13 @@
+package ua.english.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSchoolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSchoolManagerApplication.class, args);
+	}
+
+}
